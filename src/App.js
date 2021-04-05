b059de598910cf3c2bde91ae04ebdd23ca2df844
Minh Do <cl.nhatminh@gmail.com>
2021-04-05 16:51:33 +0700

    add scss and module

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import classes from './App.module.scss'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="text">
+          Sass Variables
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p className={classes.App}>
+          Sass Module
+        </p>
       </header>
     </div>
   );
